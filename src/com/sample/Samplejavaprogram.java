a0a277a95ba8dcc564ff87a9a564146b3e60a48d
Javed_Git <javedakhtar8310@gmail.com>
2022-04-29 18:43:49 +0530

    Sample java project

---
+++ src/com/sample/Samplejavaprogram.java
@@ -0,0 +1,12 @@
+package com.sample;
+
+public class Samplejavaprogram {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println("voicera analytics private limited");
+		}
+	}
+
+}
